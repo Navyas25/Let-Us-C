b13867d4c3b8249b50dd04255c6277f55a2eb680
Navya Bhushan <bhushanavya2@gmail.com>
2026-08-15 11:52:34 +0530

    Calculate gross salary based on basic salary

---
+++ prob1.1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+//da=40% of basic salary,hra=20% of basic salary,calc gross salary
+//gross salary=basic salary+da+hra
+int main(){
+  int bs,hra,da,gs;
+  printf("Enter the basic salary:");
+  scanf("%d",&bs);
+	da=bs*40/100;
+  hra=bs*20/100;
+  gs=bs+da+hra;
+  printf("gross salary is:%d",gs);
+  return 0;
+}
