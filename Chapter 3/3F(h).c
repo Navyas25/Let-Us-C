f81071b524581fd3c12b57a44d0569ecdfc535c7
Navya Bhushan <bhushanavya2@gmail.com>
2026-09-04 06:59:00 +0530

    Implement function to determine axis position

--- Chapter 3/3F(h).c
+++ Chapter 3/3F(h).c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+void onaxes(int x, int y)
+{
+  if(x==0&&y!=0)
+    printf("on y axis");
+  else if(x!=0&&y==0)
+    printf("on x axis");
+  else if(x==0&&y==0)
+    printf("on origion");
+  else
+    printf("not on axes or origion");
+}
 
+int main()
+{
+    int x, y;
+    printf("Enter x, y: ");
+    scanf("%d %d", &x, &y);
+    onaxes(x,y);
+    return 0;
+}
