fe7194e0231cef9a03d8c0c202f47c307fa686c2
Navya Bhushan <bhushanavya2@gmail.com>
2026-09-20 22:26:05 +0530

    Add full pyramid printing examples in C
    Implement three methods to print full pyramids in C.

---
+++ Chapter 6/Patter printing/full-pyramid.c
@@ -0,0 +1,61 @@
+//three ways
+//full pyramid
+/*  * 
+   * * 
+  * * * 
+ * * * * 
+* * * * *   */
+#include <stdio.h>
+int main()
+{
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=6-i;j++){//to print an inverted right pyramid of blanks
+            printf(" ");
+        }
+        for(int k=1;k<=i;k++){                                                                  
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+//full pyramid
+/*  *
+   ***
+  *****
+ *******
+*********   */
+#include <stdio.h>
+int main()
+{
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=5-i;j++){//to print an inverted right pyramid of blanks
+            printf(" ");
+        }
+        for(int k=1;k<2*i;k++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+//full pyramid
+/*      * 
+      * * * 
+    * * * * * 
+  * * * * * * * 
+* * * * * * * * *    */
+#include <stdio.h>
+int main()
+{
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=2*(5-i);j++){//to print an inverted right pyramid of blanks
+            printf(" ");
+        }
+        for(int k=1;k<2*i;k++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
