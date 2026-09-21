85d8fc66a5e86192c65a70d5e7dc3db2f05d6de0
Navya Bhushan <bhushanavya2@gmail.com>
2026-09-21 21:25:21 +0530

    Add inverted full pyramid pattern in C
    Implement inverted full pyramid pattern printing.

---
+++ Chapter 6/Patter printing/inverted-full-pyramid(3).c
@@ -0,0 +1,19 @@
+//inverted pyramid
+/*   *********
+      *******
+       *****
+        ***
+         *     */
+#include<stdio.h>
+int main(){
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=i-1;j++){
+            printf(" ");
+        }
+        for(int k=1;k<=2*(6-i)-1;k++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
