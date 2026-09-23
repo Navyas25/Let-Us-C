4cc661d5ad017fdcba3b8990954c33a60c45f5d4
Navya Bhushan <bhushanavya2@gmail.com>
2026-09-23 21:09:49 +0530

    Add hollow square pyramid pattern in C

---
+++ Chapter 6/Patter printing/hollow-square-pyramid.c
@@ -0,0 +1,21 @@
+//hollow square pyramid
+#include <stdio.h>
+int main()
+{
+    for(int i=1;i<=5;i++){
+        if(i==1||i==5){
+            for(int j=1;j<=5;j++){
+                printf("* ");
+            }
+        }
+        else{
+            printf("* ");
+            for(int j=1;j<=6;j++){
+                printf(" ");
+            }
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
