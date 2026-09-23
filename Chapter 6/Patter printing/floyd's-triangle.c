6f877fd6d306c15045d3e9452e0d6e0d65d6658e
Navya Bhushan <bhushanavya2@gmail.com>
2026-09-23 22:21:13 +0530

    Add Floyd's triangle implementation in C
    Implement Floyd's triangle printing in C.

---
+++ Chapter 6/Patter printing/floyd's-triangle.c
@@ -0,0 +1,20 @@
+//floyd's triangle
+/*
+
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+
+  */
+#include<stdio.h>
+int main(){
+    int k=1;
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<=i;j++){
+            printf("%d ",k++);
+        }
+        printf("\n");
+    }
+    return 0;
+}
